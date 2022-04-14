e65776016754fc36644d2687be7140d05c64c45a
Slava Dmitriev <d2c.tool@gmail.com>
2022-04-14 23:10:49 +0300

    added some changes

--- models/user.js
+++ models/user.js
@@ -3,19 +3,38 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
+    required: false,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: /^https*:\/\/(www.)*\S*/,
+  },
+  email: {
+    type: String,
     required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        validator.isEmail(v);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
   },
 });
 
