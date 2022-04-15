81758deef26ff5236e95ef33112d346dfb0869cf
Slava Dmitriev <d2c.tool@gmail.com>
2022-04-15 21:12:05 +0300

    small changes

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -4,7 +4,7 @@ const { ObjectId } = require('mongoose').Types;
 
 exports.signUpValidation = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().custom((e) => validator.isEmail(e)),
+    email: Joi.string().email().required(),
     password: Joi.string().required(),
     // name: Joi.string().min(2).max(30),
     // about: Joi.string().min(2).max(30),
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 // const { route } = require('./users');
-const { signUpValidation } = require('../middlewares/validation');
-const { createUser } = require('../controllers/users');
+const { signUpValidation, signInValidation } = require('../middlewares/validation');
+const { createUser, login } = require('../controllers/users');
 
 const NotFoundError = require('../errors/notFoundError');
 
@@ -11,6 +11,7 @@ router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 
 router.post('/signup', express.json(), signUpValidation, createUser);
+router.post('/signin', express.json(), signInValidation, login);
 
 router.use((req, res) => next(new NotFoundError('Что-то пошло не так')));
 
